f7730d656a3c6d8d5b9e2c5f5f201f33333d406a
galyan <galdim176@gmail.com>
2020-08-16 14:07:03 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+const cat = 'black';
+console.log(`https://irl.com/${cat}/5`);
+
+const user = 'olya';
+alert(`hi, ${user}`);
+
+
+let incr = 10,
+    decr = 10;
+
+//++incr; префиксный
+//--decr; 
+//console.log(incr, decr);
+//console.log(++incr, --decr);
+
+console.log(2+2*2 != '8'); //сравнение по значению, а не по типу данных
+const a = false,
+      b = true;
+console.log(a || !b);
+
+
